1cd8c72b3711e1275206a5affa6404bf51f603ee
Daniel Bonfim <daniel.bonfim@cubos.io>
2019-10-22 17:07:28 -0300

    scrollable

--- lib/Home.dart
+++ lib/Home.dart
@@ -17,62 +17,74 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.blue,
       ),
       body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 32),
-              child: Image.asset("images/logo.png"),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Text(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset("images/logo.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
                   "Saiba qual a melhor opção para abastecimento do seu carro.",
                   style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold
-                  ),
-              ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Preço Alcool, ex: 1,59"
-              ),
-              style: TextStyle(
-                fontSize: 22
-              ),
-              controller: _controllerAlcool,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Preço Gasolina, ex: 2,59"
-              ),
-              style: TextStyle(
-                  fontSize: 22
-              ),
-              controller: _controllerGasolina,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 30),
-              child: RaisedButton(
-                color: Colors.blue,
-                textColor: Colors.white,
-                padding: EdgeInsets.all(15),
-                child: Text(
-                    "Calcular",
-                  style: TextStyle(
-                    fontSize: 20
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
                   ),
                 ),
-                onPressed: () {
-
-                },
               ),
-            )
-          ],
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Alcool, ex: 1.59"
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerAlcool,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Gasolina, ex: 2.59"
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(
+                        fontSize: 20
+                    ),
+                  ),
+                  onPressed: () {
+
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  "Resultado",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
 
